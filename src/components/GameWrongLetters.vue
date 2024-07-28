9214128aab9aade3c16edbf4c647c918822a2d45
Alex <xinfoz19@gmail.com>
2024-07-28 13:11:17 +0400

    feat: The first version of the game is implemented

---
+++ src/components/GameWrongLetters.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+    wrongLetters: string[]
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <div 
+        class="wrong-letters-container"
+        v-if="wrongLetters.length" 
+    >
+        <div class="wrong-letters">
+            <p>Ошибки</p>
+            <span v-for="(letter, index) in wrongLetters" key="letter">
+                {{ index === wrongLetters.length - 1 ? letter : `${letter}, ` }}
+            </span>
+        </div>
+    </div>  
+</template>
