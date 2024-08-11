812e18226ac16ca6fb73c8201f43378b66b2666e
Alex <xinfoz19@gmail.com>
2024-08-11 17:44:13 +0400

    refactor: Moved the function that makes a request to the server to the api directory

---
+++ src/api/getRandomName.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const getRandomName = async () => {
+  const { data } = await axios.get<{ FirstName: string }>(
+    'https://api.randomdatatools.ru/?unescaped=false&params=FirstName'
+  )
+  return data.FirstName
+}
